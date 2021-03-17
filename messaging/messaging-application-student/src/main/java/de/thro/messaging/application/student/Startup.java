18a0b1db3c44b74584969f5b2d422af210947cea
studlinnth6233 <thomas.linner@stud.fh-rosenheim.de>
2021-03-17 19:56:01 +0100

    Created gradle multiproject (commons / application-student / application-teacher)

---
+++ messaging/messaging-application-student/src/main/java/de/thro/messaging/application/student/Startup.java
@@ -0,0 +1,9 @@
+package de.thro.messaging.application.student;
+
+public class Startup
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello from application student!");
+  }
+}
---
+++ messaging/messaging-application-teacher/src/main/java/de/thro/messaging/application/teacher/Startup.java
@@ -0,0 +1,9 @@
+package de.thro.messaging.application.teacher;
+
+public class Startup
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello from application teacher!");
+  }
+}
