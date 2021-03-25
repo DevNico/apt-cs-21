0bf1e07956747f2e763d31dfc02ea3984f8b8b5d
studlanzpa1910 <patrick.lanzinger@stud.th-rosenheim.de>
2021-03-25 17:42:20 +0000

    Update IMessage.java

--- messaging/messaging-commons/src/main/java/de/thro/messaging/commons/domain/IMessage.java
+++ messaging/messaging-commons/src/main/java/de/thro/messaging/commons/domain/IMessage.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public interface IMessage {
     IUser getSender ();
-    IUser getReceiver ();
+    IUser getReciever ();
     LocalDateTime getTime();
     String getMessageText();
 }
