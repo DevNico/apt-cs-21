1c3bb21297911ca6cda07a45d8f83554fb38725b
studeckstr1193 <tristanbenedikttorben.eckstaller@stud.fh-rosenheim.de>
2021-03-26 15:45:18 +0100

    update Interface IConfigHandler

--- messaging/messaging-commons/src/main/java/de/thro/messaging/commons/confighandler/IConfigHandler.java
+++ messaging/messaging-commons/src/main/java/de/thro/messaging/commons/confighandler/IConfigHandler.java
@@ -4,32 +4,29 @@ package de.thro.messaging.commons.confighandler;
  * Dieses Interface stellt die Schnittstelle zum ConfigHandler bereit.
  * @author Tristan Eckstaller
  */
-public interface IConfigHandler {
+public interface IConfigHandler<T> {
     /**
      * Liest Config-File aus.
      * @param path beschreibt den Dateienpfad als String, aus dem gelesen werden soll
-     * @param <T> welches Objekt soll ausgelesen werden
      * @return gibt Objekt vom Typ T zurück
      * @throws ConfigHandlerException Exception falls es Probleme beim lesen der Configuration gab
      */
-        <T> T readConfig(String path) throws ConfigHandlerException;
+        T readConfig(String path) throws ConfigHandlerException;
 
     /**
      * schreibt T in Config-File im Pfad path
      * @param path Dateienpfad als String, in den Gespeichert werden soll
      * @param fileToSerialize Datei, die als Configdatei persistiert werden soll
-     * @param <T> welches Objekt soll persistiert werden
      * @throws ConfigHandlerException Exception falls es Probleme beim schreiben der Configuration gab
      */
-        <T> void writeConfig(String path, T fileToSerialize) throws ConfigHandlerException;
+        void writeConfig(String path, T fileToSerialize) throws ConfigHandlerException;
 
     /**
      * Überprüft ob bereits eine Configdatei für T existiert
      * @param path gibt den Dateipfad an
      * @param file Datei die überprüft werden soll
-     * @param <T> welches Objekt soll darauf geprüft werden ob eine Configdatei davon existiert?
      * @return boolean-Wert ob es die Configdatei gibt
      * @throws ConfigHandlerException Exception falls es Probleme beim lesen der Configuration gab
      */
-        <T> boolean isFileAvailable(String path, T file) throws ConfigHandlerException;
+        boolean isFileAvailable(String path, T file) throws ConfigHandlerException;
 }
