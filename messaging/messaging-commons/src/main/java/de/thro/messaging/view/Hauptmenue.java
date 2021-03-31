14124a67c9f141701cb7d656fc31b5efde64c004
studlanzpa1910 <patrick.lanzinger@stud.fh-rosenheim.de>
2021-03-31 12:30:32 +0200

    Ein wenig Doku damit man es auch versteht

--- messaging/messaging-commons/src/main/java/de/thro/messaging/view/Hauptmenue.java
+++ messaging/messaging-commons/src/main/java/de/thro/messaging/view/Hauptmenue.java
@@ -12,6 +12,10 @@ public class Hauptmenue {
 
     enum UseCase{DirectMessage, Broadcast, ReadMeassage}
 
+    /*
+    Hier wird das Hauptmenü geöfnet und alle weiteren UC verwaltet.
+    Nach jedem UC kehrt das Programm zum Hauptmenü zurück.
+     */
     public static class MenueManagement{
         public void start(){
             boolean end = false;
@@ -34,6 +38,10 @@ public class Hauptmenue {
         }
 
 
+        /**
+         * Was soll passieren, wenn der UC directMessage gerufen wird.
+         * Hier kann die Logik für Direktnachrichten hin.
+         */
         private  void directMessage() {
             System.out.println("Write your direct message");
             try {
@@ -43,6 +51,10 @@ public class Hauptmenue {
             }
         }
 
+        /**
+         * Was soll passieren, wenn der UC broadcast gerufen wird.
+         * Hier kann die Logik für Broadcasts rein.
+         */
         private  void broadcast() {
             System.out.println("Write your broadcast message");
             try {
@@ -52,6 +64,11 @@ public class Hauptmenue {
             }
         }
 
+
+        /**
+         * Was soll passieren, wenn der UC readMessage gerufen wird.
+         * Hier kann die Logik für readMessage rein.
+         */
         private  void readMessage() {
             System.out.println("These are your messages");
             try {
@@ -61,6 +78,10 @@ public class Hauptmenue {
             }
         }
 
+        /**
+         * Das Tatsächliche Hauptmenü. Hier wird der Menütext ausgegeben und auf eine Usereingabe gewartet.
+         * @return Usecase der angibt was der User tun möchte.
+         */
         private UseCase mainMenue(){
             while (true){
                 // create a BufferedReader using System.in
@@ -83,6 +104,10 @@ public class Hauptmenue {
             }
         }
 
+        /**
+         * Allgemeiner Anfang für jede neue Seite.
+         * @param Description Was soll im head stehen.
+         */
         private  void head(String Description){
             System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
             System.out.println(Description);
