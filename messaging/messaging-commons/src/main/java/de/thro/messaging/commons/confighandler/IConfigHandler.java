e2319f39bccfb3689bcd55e8d07cda57fbf9e579
studeckstr1193 <tristanbenedikttorben.eckstaller@stud.fh-rosenheim.de>
2021-03-31 17:47:37 +0200

    edited description to match new method Head isFileAvailable

--- messaging/messaging-commons/src/main/java/de/thro/messaging/commons/confighandler/IConfigHandler.java
+++ messaging/messaging-commons/src/main/java/de/thro/messaging/commons/confighandler/IConfigHandler.java
@@ -39,7 +39,6 @@ public interface IConfigHandler<T> {
     /**
      * Überprüft ob bereits eine Configdatei für T existiert
      * @param path gibt den Dateipfad an. Wenn null oder leerer String wird ein Pfad im Userverzeichnis abgecheckt
-     * @param file Datei die überprüft werden soll
      * @return boolean-Wert ob es die Configdatei gibt
      * @throws ConfigHandlerException Exception falls es Probleme beim lesen der Configuration gab
      */
@@ -47,7 +46,6 @@ public interface IConfigHandler<T> {
 
     /**
      * Überprüft ob bereits eine Configdatei für T existiert. Man muss keinen Pfad angeben.
-     * @param file Datei die überprüft werden soll
      * @return boolean-Wert ob es die Configdatei gibt
      * @throws ConfigHandlerException Exception falls es Probleme beim lesen der Configuration gab
      */
