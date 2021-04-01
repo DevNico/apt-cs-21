1b0a0ddddd03b64abf48ccf4fc3336de2a721483
studlinnth6233 <thomas.linner@stud.fh-rosenheim.de>
2021-04-01 12:54:55 +0200

    Removed projects application-student and application-teacher Created project application
    Issue: #23

--- messaging/messaging-application-student/src/main/java/de/thro/messaging/application/student/Startup.java
+++
@@ -1,9 +0,0 @@
-package de.thro.messaging.application.student;
-
-public class Startup
-{
-  public static void main(String[] args)
-  {
-    System.out.println("Hello from application student!");
-  }
-}
---
+++ messaging/messaging-application/src/main/java/de/thro/messaging/application/Startup.java
@@ -0,0 +1,9 @@
+package de.thro.messaging.application;
+
+public class Startup
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Starting application ...");
+  }
+}
