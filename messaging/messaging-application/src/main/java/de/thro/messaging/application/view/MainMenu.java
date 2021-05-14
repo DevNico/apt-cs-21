b755a89c0a74ffc541d8ada1c78165996143bc2b
Vitus Henkel <vitus.henkel@stud.th-rosenheim.de>
2021-05-14 16:07:23 +0200

    #33 Auto-formatting and troubleshooting while loops
    The termination conditions of the while loops were never thrown or used. The bug has been fixed.

--- messaging/messaging-application/src/main/java/de/thro/messaging/application/view/MainMenu.java
+++ messaging/messaging-application/src/main/java/de/thro/messaging/application/view/MainMenu.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class MainMenu {
 
-    enum UseCase{DirectMessage, Broadcast, ReadMessage, EndApp}
+    enum UseCase {DirectMessage, Broadcast, ReadMessage, EndApp}
 
     /*
     Hier wird das Hauptmenü geöffnet und alle weiteren UC verwaltet.
@@ -19,193 +19,198 @@ public class MainMenu {
      */
 
 
-        private ViewController vc;
+    private ViewController vc;
 
-        public MainMenu(ViewController vc){
-            this.vc = vc;
-        }
+    public MainMenu(ViewController vc) {
+        this.vc = vc;
+    }
 
-        /**
-         * Startet Menüführung für einen Studenten.
-         */
-        public void startStudent(){
-            boolean end = false;
-            while (!end){
-                UseCase uc = mainMenu(true);
-                switch (uc){
-                    case DirectMessage:
-                        directMessage();
-                        break;
-                    case Broadcast:
-                        broadcast();
-                        break;
-                    case ReadMessage:
-                        readMessage();
-                        break;
-                    case EndApp:
-                        endApp();
-                        break;
-                    default:
-                        System.out.println("Das ist kein Menü");
-                }
+    /**
+     * Startet Menüführung für einen Studenten.
+     */
+    public void startStudent() {
+        var isInterrupted = false;
+        while (!isInterrupted) {
+            UseCase uc = mainMenu(true);
+            switch (uc) {
+                case DirectMessage:
+                    directMessage();
+                    break;
+                case Broadcast:
+                    broadcast();
+                    break;
+                case ReadMessage:
+                    readMessage();
+                    break;
+                case EndApp:
+                    isInterrupted = true;
+                    endApp();
+                    break;
+                default:
+                    System.out.println("Das ist kein Menü");
             }
-        }
-
-        /**
-         * Startet Menüführung für einen Professor.
-         */
-        public void startTeacher(){
-            boolean end = false;
-            while (!end){
-                UseCase uc = mainMenu(false);
-                switch (uc){
-                    case Broadcast:
-                        broadcast();
-                        break;
-                    case EndApp:
-                        endApp();
-                        break;
-                    default:
-                        System.out.println("Das ist kein Menü");
-                }
-            }
-        }
-
-
-
-        //Ein reader der für alle Usecases verwendet werden kann
-        BufferedReader readerForUc = new BufferedReader(new InputStreamReader(System.in));
-        /**
-         * App wird Beendet.
-         */
-        private void endApp(){
-            System.out.println("App Beendet.");
-            vc.endApp();
-        }
-
-        /**
-         * Was soll passieren, wenn der UC directMessage gerufen wird.
-         * Hier kann die Logik für Direktnachrichten hin.
-         */
-        private  void directMessage() {
-            String receiver = "";
-            String messageText = "";
-            System.out.println("Schreiben Sie Ihre Nachricht und bestätigen Sie mit 'Enter'");
-            try {
-                messageText = readerForUc.readLine();
-                System.out.println("Bitte geben Sie einen Empfänger ein. Danach wird die Nachricht versendet.");
-                receiver = readerForUc.readLine();
-            } catch (IOException e) {
-                //Sollte die Eingabe ungültig sein, kehrt das System zum Hauptmenü zurück
-                System.out.println("Das war keine Korrekte eingabe.");
-                return;
-            }
-            IMessage message = vc.createDirektMessage(receiver, messageText);
-            vc.sendDirect(message);
-        }
-
-        /**
-         * Was soll passieren, wenn der UC broadcast gerufen wird.
-         * Hier kann die Logik für Broadcasts rein.
-         */
-        private  void broadcast() {
-            String messageText = "";
-            System.out.println("Schreiben Sie Ihre Rundnachricht und versenden Sie mit 'Enter'");
-            try {
-                messageText = readerForUc.readLine();
-            } catch (IOException e) {
-                System.out.println("Das war keine Korrekte eingabe.");
-            }
-            IMessage message = vc.createBroadcastMessage(messageText);
-            vc.sendBroadcast(message);
-        }
-
-
-        /**
-         * Was soll passieren, wenn der UC readMessage gerufen wird.
-         * Hier kann die Logik für readMessage rein.
-         */
-        private  void readMessage() {
-            System.out.println("Das sind Ihre Nachrichten");
-            List<IMessage> messages = vc.displayReceivedMessages();
-            for (IMessage m: messages) {
-                System.out.println(m);
-            }
-        }
-
-        /**
-         * Das tatsächliche Hauptmenü. Hier wird der Menütext ausgegeben und auf eine Usereingabe gewartet.
-         * @return Usecase der angibt was der User tun möchte.
-         */
-        private UseCase mainMenu(boolean isStudent){
-            while (true){
-                // create a BufferedReader using System.in
-                BufferedReader obj = new BufferedReader(new InputStreamReader(System.in));
-                String input = "";
-                head("Hauptmenü");
-
-                //ist der Nutzer ein Student?
-                if(isStudent) {
-                    mainMenuTextStudent();
-                    try {
-                        input = obj.readLine();
-                    } catch (IOException e) {
-                    }
-
-                    switch (input.toUpperCase()) {
-                        case "D":
-                            return UseCase.DirectMessage;
-                        case "B":
-                            return UseCase.Broadcast;
-                        case "R":
-                            return UseCase.ReadMessage;
-                        case "X":
-                            return UseCase.EndApp;
-                        default:
-                            System.out.println("Das ist kein Menü");
-                            break;
-                    }
-                }else{
-                    mainMenuTextTeacher();
-                    try {
-                        input = obj.readLine();
-                    } catch (IOException e) {
-                    }
-
-                    switch (input.toUpperCase()) {
-                        case "B":
-                            return UseCase.Broadcast;
-                        case "X":
-                            return UseCase.EndApp;
-                        default:
-                            System.out.println("Das ist kein Menü");
-                            break;
-                    }
-                }
-            }
-        }
-
-
-        /**
-         * Allgemeiner Anfang für jede neue Seite.
-         * @param Description Was soll im Head stehen?
-         */
-        private  void head(String Description){
-            System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
-            System.out.println(Description);
-            System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
-        }
-
-        private void mainMenuTextStudent(){
-            System.out.println("D um eine Direktnachricht zu schreiben");
-            System.out.println("B um eine Rundnachricht zu schreiben");
-            System.out.println("R um die Nachrichten anzuzeigen");
-            System.out.println("X um Programm zu beenden");
-        }
-
-        private void mainMenuTextTeacher(){
-            System.out.println("B um eine Rundnachricht zu schreiben");
-            System.out.println("X um Programm zu beenden");
         }
     }
+
+    /**
+     * Startet Menüführung für einen Professor.
+     */
+    public void startTeacher() {
+        var isInterrupted = false;
+        do {
+            UseCase uc = mainMenu(false);
+            switch (uc) {
+                case Broadcast:
+                    broadcast();
+                    break;
+                case EndApp:
+                    isInterrupted = true;
+                    endApp();
+                    break;
+                default:
+                    System.out.println("Das ist kein Menü");
+            }
+        }
+        while (isInterrupted);
+
+    }
+
+    //Ein reader der für alle Usecases verwendet werden kann
+    BufferedReader readerForUc = new BufferedReader(new InputStreamReader(System.in));
+
+    /**
+     * App wird Beendet.
+     */
+    private void endApp() {
+        System.out.println("App Beendet.");
+        vc.endApp();
+    }
+
+    /**
+     * Was soll passieren, wenn der UC directMessage gerufen wird.
+     * Hier kann die Logik für Direktnachrichten hin.
+     */
+    private void directMessage() {
+        String receiver = "";
+        String messageText = "";
+        System.out.println("Schreiben Sie Ihre Nachricht und bestätigen Sie mit 'Enter'");
+        try {
+            messageText = readerForUc.readLine();
+            System.out.println("Bitte geben Sie einen Empfänger ein. Danach wird die Nachricht versendet.");
+            receiver = readerForUc.readLine();
+        } catch (IOException e) {
+            //Sollte die Eingabe ungültig sein, kehrt das System zum Hauptmenü zurück
+            System.out.println("Das war keine Korrekte eingabe.");
+            return;
+        }
+        IMessage message = vc.createDirektMessage(receiver, messageText);
+        vc.sendDirect(message);
+    }
+
+    /**
+     * Was soll passieren, wenn der UC broadcast gerufen wird.
+     * Hier kann die Logik für Broadcasts rein.
+     */
+    private void broadcast() {
+        String messageText = "";
+        System.out.println("Schreiben Sie Ihre Rundnachricht und versenden Sie mit 'Enter'");
+        try {
+            messageText = readerForUc.readLine();
+        } catch (IOException e) {
+            System.out.println("Das war keine Korrekte eingabe.");
+        }
+        IMessage message = vc.createBroadcastMessage(messageText);
+        vc.sendBroadcast(message);
+    }
+
+
+    /**
+     * Was soll passieren, wenn der UC readMessage gerufen wird.
+     * Hier kann die Logik für readMessage rein.
+     */
+    private void readMessage() {
+        System.out.println("Das sind Ihre Nachrichten");
+        List<IMessage> messages = vc.displayReceivedMessages();
+        for (IMessage m : messages) {
+            System.out.println(m);
+        }
+    }
+
+    /**
+     * Das tatsächliche Hauptmenü. Hier wird der Menütext ausgegeben und auf eine Usereingabe gewartet.
+     *
+     * @return Usecase der angibt was der User tun möchte.
+     */
+    private UseCase mainMenu(boolean isStudent) {
+        while (true) {
+            // create a BufferedReader using System.in
+            BufferedReader obj = new BufferedReader(new InputStreamReader(System.in));
+            String input = "";
+            head("Hauptmenü");
+
+            //ist der Nutzer ein Student?
+            if (isStudent) {
+                mainMenuTextStudent();
+                try {
+                    input = obj.readLine();
+                } catch (IOException e) {
+                }
+
+                switch (input.toUpperCase()) {
+                    case "D":
+                        return UseCase.DirectMessage;
+                    case "B":
+                        return UseCase.Broadcast;
+                    case "R":
+                        return UseCase.ReadMessage;
+                    case "X":
+                        return UseCase.EndApp;
+                    default:
+                        System.out.println("Das ist kein Menü");
+                        break;
+                }
+            } else {
+                mainMenuTextTeacher();
+                try {
+                    input = obj.readLine();
+                } catch (IOException e) {
+                }
+
+                switch (input.toUpperCase()) {
+                    case "B":
+                        return UseCase.Broadcast;
+                    case "X":
+                        return UseCase.EndApp;
+                    default:
+                        System.out.println("Das ist kein Menü");
+                        break;
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Allgemeiner Anfang für jede neue Seite.
+     *
+     * @param Description Was soll im Head stehen?
+     */
+    private void head(String Description) {
+        System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+        System.out.println(Description);
+        System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+    }
+
+    private void mainMenuTextStudent() {
+        System.out.println("D um eine Direktnachricht zu schreiben");
+        System.out.println("B um eine Rundnachricht zu schreiben");
+        System.out.println("R um die Nachrichten anzuzeigen");
+        System.out.println("X um Programm zu beenden");
+    }
+
+    private void mainMenuTextTeacher() {
+        System.out.println("B um eine Rundnachricht zu schreiben");
+        System.out.println("X um Programm zu beenden");
+    }
+}
 
