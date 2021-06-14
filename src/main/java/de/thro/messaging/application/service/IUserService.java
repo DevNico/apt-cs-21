846f891844488b7427b6736d1d633061e08536a6
simon <simon.maier_1@stud.th-rosenheim.de>
2021-06-14 21:43:05 +0200

    #44 return Typ der getUserName Methode auf User-Object geändert

--- src/main/java/de/thro/messaging/application/service/IUserService.java
+++ src/main/java/de/thro/messaging/application/service/IUserService.java
@@ -1,10 +1,11 @@
 package de.thro.messaging.application.service;
 
 import de.thro.messaging.domain.enums.UserType;
+import de.thro.messaging.domain.models.User;
 
 public interface IUserService {
 
-    String getUserName();
+    User getUserName();
     void setUserName(String name);
     UserType getUserType();
     void setUserType(UserType type);
