22f3f76550d31f80276193f89f32f465a5d556ab
Carina Wehr <carina.wehr@stud.fh-rosenheim.de>
2021-06-27 13:53:01 +0200

    49-itshouldsendadirektmessage test wurde angepasst mit dem richtigen datetimefactory

--- src/test/java/de/thro/application/service/ChatServiceTest.java
+++ src/test/java/de/thro/application/service/ChatServiceTest.java
@@ -6,9 +6,12 @@ import de.thro.messaging.application.dependencies.messagequeue.exceptions.Messag
 import de.thro.messaging.application.dependencies.messagequeue.exceptions.MessageQueueFetchException;
 import de.thro.messaging.application.dependencies.messagequeue.exceptions.MessageQueueSendException;
 import de.thro.messaging.application.exceptions.ApplicationException;
+import de.thro.messaging.application.exceptions.UserAlreadyExistsException;
 import de.thro.messaging.application.service.ChatService;
 import de.thro.messaging.application.service.IChatService;
+import de.thro.messaging.application.service.IUserManager;
 import de.thro.messaging.common.DateTimeFactory;
+import de.thro.messaging.common.IDateTimeFactory;
 import de.thro.messaging.common.confighandler.ConfigHandlerException;
 import de.thro.messaging.domain.enums.UserType;
 import de.thro.messaging.domain.exceptions.UserNotExistsException;
@@ -29,38 +32,43 @@ import static org.mockito.Mockito.*;
 public class ChatServiceTest {
 
     @Test
-    public void itShouldSendADirectMessage() throws ApplicationException, MessageQueueConnectionException, MessageQueueConfigurationException, MessageQueueSendException {
+    public void itShouldSendADirectMessage() throws ApplicationException, MessageQueueConnectionException, MessageQueueConfigurationException, MessageQueueSendException, UserNotExistsException, ConfigHandlerException, UserAlreadyExistsException {
         /*  We need mocks for the following services:
          *  IMessageQueue messageQueueMock,
          *  IUserService userServiceMock
          */
         IMessageQueue messageQueueMock = mock(IMessageQueue.class);
+        IDateTimeFactory dateTimeFactory = mock(IDateTimeFactory.class);
         /*
          * Next up we configure the mocks. This is phase one, where we capture (program) a behaviour
          * so we can later replay it.
          */
-        final String userMail = UUID.randomUUID().toString();
+        final String userName = UUID.randomUUID().toString();
         final String receiver = UUID.randomUUID().toString();
-        final User user = new User(userMail, UserType.STUDENT);
+        final UserType userType = UserType.STUDENT;
+        final User user = new User(userName, userType);
         final String messageText = UUID.randomUUID().toString();
+        final var localDateTime = LocalDateTime.now();
 
-        Message message = new Message(user, receiver, false, messageText, DateTimeFactory.getDateTime());
+        when(dateTimeFactory.getDateTime()).thenReturn(localDateTime);
+        Message message = new Message(user, receiver, false, messageText, dateTimeFactory.getDateTime());
         // We need to define the object that will be passed to the messageQueueMock.
         doNothing().when(messageQueueMock).sendDirect(message);
         // Let's create the service and execute the sendDirectMessage function
-        IChatService chatService = new ChatService(messageQueueMock, user);
+        IChatService chatService = new ChatService(messageQueueMock, user, dateTimeFactory);
         chatService.sendDirectMessage(receiver, messageText);
         verify(messageQueueMock, times(1)).sendDirect(message);
         verify(messageQueueMock, times(0)).sendBroadcast(any(Message.class));
     }
 
-    @Test
+        @Test
     void itShouldGetDirectMessages() throws MessageQueueFetchException, MessageQueueConnectionException, MessageQueueConfigurationException, ApplicationException, ConfigHandlerException, UserNotExistsException {
         /*  We need mocks for the following services:
          *  IMessageQueue messageQueueMock,
          *  IUserService userServiceMock
          */
         IMessageQueue messageQueueMock = mock(IMessageQueue.class);
+        IDateTimeFactory dateTimeFactory = mock(IDateTimeFactory.class);
 
         final String userName = UUID.randomUUID().toString();
         final UserType userType = UserType.TEACHER;
@@ -85,10 +93,11 @@ public class ChatServiceTest {
             broadcastMessages.add(message);
         }
 
+        when(dateTimeFactory.getDateTime()).thenReturn(localDateTime);
         when(messageQueueMock.getDirectMessages(any())).thenReturn(directMessages);
         when(messageQueueMock.getBroadcastMessages()).thenReturn(broadcastMessages);
 
-        ChatService chatService = new ChatService(messageQueueMock, user);
+        ChatService chatService = new ChatService(messageQueueMock, user, dateTimeFactory);
         List<Message> messages = chatService.getMessages();
 
         verify(messageQueueMock, times(1)).getDirectMessages(user.getName());
@@ -110,6 +119,7 @@ public class ChatServiceTest {
          *  IUserService userServiceMock
          */
         IMessageQueue messageQueueMock = mock(IMessageQueue.class);
+        IDateTimeFactory dateTimeFactory = mock(IDateTimeFactory.class);
 
         final String userName = UUID.randomUUID().toString();
         final UserType userType = UserType.STUDENT;
@@ -131,10 +141,11 @@ public class ChatServiceTest {
             broadcastMessages.add(new Message(user, receiver, true, Integer.toString(i), localDateTime));
         }
 
+        when(dateTimeFactory.getDateTime()).thenReturn(localDateTime);
         when(messageQueueMock.getDirectMessages(any())).thenReturn(new ArrayList<>(directMessages));
         when(messageQueueMock.getBroadcastMessages()).thenReturn(new ArrayList<>(broadcastMessages));
 
-        ChatService chatService = new ChatService(messageQueueMock, user);
+        ChatService chatService = new ChatService(messageQueueMock, user, dateTimeFactory);
         List<Message> messages = chatService.getMessages();
 
         verify(messageQueueMock, times(1)).getDirectMessages(user.getName());
